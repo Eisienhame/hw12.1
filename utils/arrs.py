e189ecce0ff5cd073d9b94cd8fbc5036d229c256
Eisiehame <knurov24@gmail.com>
2023-01-30 20:11:07 +0200

    fun add

--- utils/arrs.py
+++ utils/arrs.py
@@ -13,6 +13,9 @@ def get(array, index, default=None):
     """
     if index < 0:
         return default
+    else:
+        if array == []:
+            return default
 
     return array[index]
 
